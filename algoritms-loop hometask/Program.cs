e738020177f71efa1a4cf20faad3368d403c2b52
aghzd <alizaminma@code.edu.az>
2024-10-21 12:28:17 +0400

    hometask algoritms-loop

---
+++ algoritms-loop hometask/Program.cs
@@ -0,0 +1,78 @@
+﻿// 1- Verilmiş n ve m (n<m) ədədləri arasındakı tək ədədlərin sayını tapın.
+//int n = 15;
+//int m = 20;
+//int d = 0;
+//if (n < m)
+//{
+//    for (int i = n; i < m; i++)
+//    {
+//       if (i%2==1)
+//        d++;
+
+//    }
+//    Console.WriteLine($"tek ededlerin sayi {d} dir ");
+//}
+//else
+//    Console.WriteLine("m ededi n ededinden kicikdir");
+
+
+
+//2- Verilmiş n ve m (n<m) ededleri arasindaki tek ədədlərin cəmini tapin.
+//int n = 15;
+//int m = 20;
+//int d = 0;
+//if (n < m)
+//{
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 1)
+//            d += i;
+
+//    }
+//    Console.WriteLine($"tek ededlerin cemi {d} e beraberdir ");
+//}
+//else
+//    Console.WriteLine("m ededi n ededinden kicikdir");
+
+
+
+
+
+//3- m - ədədi verilir. 1 - dən m - ədədinə kimi olan cüt ədədlərin hasilini tapin.
+//int m = 12;
+//int sum = 1;
+//for (int i = 1; i < m; i++)
+//{
+//    if (i%2 == 0)
+//        sum *= i;
+//}
+//Console.WriteLine($"1 den {m} e qeder olan cut ededlerin hasili {sum} a berarberdir");
+
+
+
+
+
+//4- a - ədədi 3-ə bölünən cüt ədəddirsə "3-e bolunur cutdur" ,3-ə bölünən tək ədəddirsə " ,3-e bolunur tekdir" əks halda "3-e bolunmur" sözünü ekranda çap edin.
+//int a = 13;
+//if ((a % 3 == 0) && (a % 2 == 0))
+//    Console.WriteLine("3 e bolunur cutdur");
+//else if ((a % 3 == 0) && (a % 2 == 1))
+//    Console.WriteLine("3 e bolunur tekdir");
+//else
+//    Console.WriteLine("eded 3 e bolunmur");
+
+
+
+
+//5-Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın.
+//int n = 14;
+//int d = 0;
+//for (int i = 1 ; i <= n; i++)
+//{
+//    if (n % i == 0)
+//        d++;
+//}
+//if (d > 2)
+//    Console.WriteLine($"{n} murekkeb ededdir");
+//else
+//    Console.WriteLine($"{n} sade ededdir");
